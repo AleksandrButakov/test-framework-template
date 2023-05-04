735eb5b0db9b5dcc4bb1fad2deb91c0fb91f9d82
AleksandrButakov <anbngm@gmail.com>
2023-05-04 12:52:24 +0500

    fix: change attach video

--- src/test/java/cloud/autotests/helpers/AllureAttachments.java
+++ src/test/java/cloud/autotests/helpers/AllureAttachments.java
@@ -35,49 +35,49 @@ public class AllureAttachments {
         return DriverUtils.getPageSourceAsBytes();
     }
 
-//    public static void addVideo(String sessionId) {
-//        URL videoUrl = DriverUtils.getVideoUrl(sessionId);
-//        if (videoUrl != null) {
-//            InputStream videoInputStream = null;
-//            sleep(1000);
-//
-//            for (int i = 0; i < 10; i++) {
-//                try {
-//                    videoInputStream = videoUrl.openStream();
-//                    break;
-//                } catch (FileNotFoundException e) {
-//                    sleep(1000);
-//                } catch (IOException e) {
-//                    LOGGER.warn("[ALLURE VIDEO ATTACHMENT ERROR] Cant attach allure video, {}", videoUrl);
-//                    e.printStackTrace();
-//                }
-//            }
-//            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
-//        }
-//    }
+    public static void addVideo(String sessionId) {
+        URL videoUrl = DriverUtils.getVideoUrl(sessionId);
+        if (videoUrl != null) {
+            InputStream videoInputStream = null;
+            sleep(1000);
 
-
-
-
-    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static String attachVideo(String sessionId) {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + getVideoUrl(sessionId)
-                + "' type='video/mp4'></video></body></html>";
-    }
-
-    public static String getVideoUrl(String sessionId) {
-        return getWebVideoUrl(sessionId);
-    }
-
-    public static String getWebVideoUrl(String sessionId) {
-        try {
-            return String.valueOf(new URL("http://62.113.108.218:8080/video/" + sessionId + ".mp4"));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+            for (int i = 0; i < 10; i++) {
+                try {
+                    videoInputStream = videoUrl.openStream();
+                    break;
+                } catch (FileNotFoundException e) {
+                    sleep(1000);
+                } catch (IOException e) {
+                    LOGGER.warn("[ALLURE VIDEO ATTACHMENT ERROR] Cant attach allure video, {}", videoUrl);
+                    e.printStackTrace();
+                }
+            }
+            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
         }
-        return null;
     }
+
+
+
+
+//    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+//    public static String attachVideo(String sessionId) {
+//        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+//                + getVideoUrl(sessionId)
+//                + "' type='video/mp4'></video></body></html>";
+//    }
+//
+//    public static String getVideoUrl(String sessionId) {
+//        return getWebVideoUrl(sessionId);
+//    }
+//
+//    public static String getWebVideoUrl(String sessionId) {
+//        try {
+//            return String.valueOf(new URL("http://62.113.108.218:8080/video/" + sessionId + ".mp4"));
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
 
 
